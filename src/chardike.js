d78361e1d28c320b83eb03f7ec4b4d4eb1b1868f
sadiyabhbb <sadiyabhbb@proton.me>
2025-09-08 22:54:34 +0600

    Create chardike.js

---
+++ src/chardike.js
@@ -0,0 +1,36 @@
+const axios = require('axios');
+
+const config = {
+  name: "chardike",
+  version: "1.0.0",
+  credits: "YOUR_NAME",
+  permissions: [0],
+  description: "Send OTP via Chardike",
+  usages: "phone",
+  cooldowns: 0
+};
+
+async function sendOtp(phone) {
+  try {
+    const res = await axios.post(
+      'https://api.chardike.com/api/otp/send',
+      {
+        phone: phone,
+        otp_type: "login"
+      },
+      {
+        headers: {
+          'Content-Type': 'application/json',
+          'Accept': '*/*',
+          'Origin': 'https://chardike.com',
+          'X-Requested-With': 'mark.via.gp'
+        }
+      }
+    );
+    return res.data;
+  } catch (error) {
+    return { error: error.response ? error.response.data : error.message };
+  }
+}
+
+module.exports = { config, sendOtp };
