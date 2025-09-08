c6516e28164be211668b59d0f20ba572897b7fd5
sadiyabhbb <sadiyabhbb@proton.me>
2025-09-08 13:15:49 +0600

    Update garibook.js

--- src/garibook.js
+++ src/garibook.js
@@ -1,51 +1,50 @@
-// src/garibook.js
 const axios = require("axios");
 
 const config = {
   name: "garibook",
+  version: "1.0.0",
+  credits: "BLACK",
+  permissions: [2],
   description: "Send OTP via Garibook API",
-  usage: "<phone>",
-  cooldown: 0,
-  type: "api",
-  run: async function (phone) {
-    try {
-      const url = "https://api.garibookadmin.com/api/v3/user/login";
+  usage: "garibook <phone>",
+  cooldowns: 0,
+};
 
-      const headers = {
-        "Host": "api.garibookadmin.com",
-        "Connection": "keep-alive",
-        "sec-ch-ua-platform": "\"Android\"",
-        "User-Agent":
-          "Mozilla/5.0 (Linux; Android 12; M2010J19CG Build/SKQ1.211202.001) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/139.0.7258.158 Mobile Safari/537.36",
-        accept: "application/json",
-        "sec-ch-ua":
-          "\"Not;A=Brand\";v=\"99\", \"Android WebView\";v=\"139\", \"Chromium\";v=\"139\"",
-        "content-type": "application/json",
-        "sec-ch-ua-mobile": "?1",
-        Origin: "https://garibook.com",
-        "X-Requested-With": "mark.via.gp",
-        "Sec-Fetch-Site": "cross-site",
-        "Sec-Fetch-Mode": "cors",
-        "Sec-Fetch-Dest": "empty",
-        Referer: "https://garibook.com/",
-        "Accept-Encoding": "gzip, deflate, br, zstd",
-        "Accept-Language": "en-US,en;q=0.9",
-      };
-
-      const body = {
+async function sendOtp(phone) {
+  try {
+    const response = await axios.post(
+      "https://api.garibookadmin.com/api/v3/user/login",
+      {
         mobile: phone,
         recaptcha_token: "garibookcaptcha",
         channel: "web",
-      };
+      },
+      {
+        headers: {
+          Host: "api.garibookadmin.com",
+          "User-Agent":
+            "Mozilla/5.0 (Linux; Android 12; M2010J19CG Build/SKQ1.211202.001) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/139.0.7258.158 Mobile Safari/537.36",
+          Accept: "application/json",
+          "Content-Type": "application/json",
+          Origin: "https://garibook.com",
+          Referer: "https://garibook.com/",
+        },
+      }
+    );
 
-      const response = await axios.post(url, body, { headers });
-      return response.data;
-    } catch (error) {
-      return {
-        error: error.response ? error.response.data : error.message,
-      };
-    }
-  },
+    return {
+      success: true,
+      message: response.data.message || "OTP sent",
+    };
+  } catch (error) {
+    return {
+      success: false,
+      error: error.response ? error.response.data : error.message,
+    };
+  }
+}
+
+module.exports = {
+  ...config,
+  sendOtp,
 };
-
-module.exports = config;
