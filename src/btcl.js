4f18a2d0333029b1ea5ab90cbb510d2ccb89acf8
sadiyabhbb <sadiyabhbb@proton.me>
2025-09-08 23:13:40 +0600

    Update btcl.js

--- src/btcl.js
+++ src/btcl.js
@@ -1,4 +1,5 @@
 const axios = require('axios');
+const qs = require('qs');
 
 const config = {
   name: "btcl",
@@ -11,20 +12,39 @@ const config = {
 };
 
 async function sendOtp(phone) {
-  const url = 'https://bdia.btcl.com.bd/client/client/registrationMobVerification-2.jsp?moduleID=1';
-  const data = `actionType=otpSend&mobileNo=${phone}`;
-
-  const headers = {
-    'Content-Type': 'application/x-www-form-urlencoded',
-    'Origin': 'https://bdia.btcl.com.bd',
-    'X-Requested-With': 'mark.via.gp'
-  };
-
   try {
-    const res = await axios.post(url, data, { headers });
-    return res.data;
-  } catch (err) {
-    return { error: err.message, details: err.response?.data || null };
+    const url = 'https://bdia.btcl.com.bd/client/client/registrationMobVerification-2.jsp';
+    const query = { moduleID: 1 };
+    
+    const data = qs.stringify({
+      actionType: 'otpSend',
+      mobileNo: phone
+    });
+
+    const headers = {
+      'User-Agent': 'Mozilla/5.0 (Linux; Android 12; M2010J19CG Build/SKQ1.211202.001) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/139.0.7258.158 Mobile Safari/537.36',
+      'Content-Type': 'application/x-www-form-urlencoded',
+      'Origin': 'https://bdia.btcl.com.bd',
+      'Referer': 'https://bdia.btcl.com.bd/client/client/registrationMobVerification-1.jsp?moduleID=1',
+      'X-Requested-With': 'mark.via.gp',
+      'Sec-Fetch-Site': 'same-origin',
+      'Sec-Fetch-Mode': 'navigate',
+      'Sec-Fetch-Dest': 'document',
+      'Cache-Control': 'max-age=0',
+      'Cookie': 'JSESSIONID=A31A19CACAFE4409ACC2C8D9CFE7DEA4'
+    };
+
+    const response = await axios.post(url, data, {
+      headers,
+      params: query
+    });
+
+    return response.data;
+  } catch (error) {
+    return {
+      error: error.message,
+      details: error.response ? error.response.data : null
+    };
   }
 }
 
